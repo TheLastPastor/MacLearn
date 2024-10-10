8127c7e6f5bb795813d7bd61dc27bb67c9040930
Pieter Timmerman <pieter@skeps.nl>
2024-10-10 13:25:08 +0200

    deel 3 set 1

--- Main/startcode_deel3-set1/uitwerkingen.py
+++ Main/startcode_deel3-set1/uitwerkingen.py
@@ -1,3 +1,5 @@
+from functools import total_ordering
+
 import numpy as np
 from tensorflow import keras
 import tensorflow as tf
@@ -5,7 +7,8 @@ import tensorflow as tf
 
 def load_model():
     # Deze methode laadt het getrainde model dat je bij de vorige opgavenset heb
-    # opgeslagen. 
+    # opgeslagen.
+    return keras.models.load_model('../startcode_deel2-set3/mnist.keras')
 
     # YOUR CODE HERE
     pass
@@ -15,13 +18,12 @@ def conf_matrix(labels, pred):
     # Retourneer de econfusion matrix op basis van de gegeven voorspelling (pred) en de actuele
     # waarden (labels). Check de documentatie van tf.math.confusion_matrix:
     # https://www.tensorflow.org/api_docs/python/tf/math/confusion_matrix
-    
-    # YOUR CODE HERE
+    return tf.math.confusion_matrix(labels, pred)
     pass
     
 
 # OPGAVE 1b
-def conf_els(conf, labels): 
+def conf_els(conf, labels):
     # Deze methode krijgt een confusion matrix mee (conf) en een set van labels. Als het goed is, is 
     # de dimensionaliteit van de matrix gelijk aan len(labels) × len(labels) (waarom?). Bereken de 
     # waarden van de TP, FP, FN en TN conform de berekening in de opgave. Maak vervolgens gebruik van
@@ -29,11 +31,20 @@ def conf_els(conf, labels):
     # als volgt is gedefinieerd:
 
     #     (categorie:string, tp:int, fp:int, fn:int, tn:int)
- 
+
     # Check de documentatie van numpy diagonal om de eerste waarde te bepalen.
     # https://numpy.org/doc/stable/reference/generated/numpy.diagonal.html
- 
-    # YOUR CODE HERE
+
+    metrics = list()
+
+    tp = conf.diagonal()
+    for i in range(len(labels)):
+        fp = sum(conf[:, i]) - tp[i]
+        fn = sum(conf[i, :]) - tp[i]
+        tn = np.sum(conf) - tp[i] - fp - fn
+        metrics.append((labels[i], tp[i], fp, fn, tn))
+
+    return metrics
     pass
 
 # OPGAVE 1c
@@ -44,14 +55,18 @@ def conf_data(metrics):
     # vorm van een dictionary (de scaffold hiervan is gegeven).
 
     # VERVANG ONDERSTAANDE REGELS MET JE EIGEN CODE
-    
-    tp = 1
-    fp = 1
-    fn = 1
-    tn = 1
+    tp, fp, fn, tn = 0, 0, 0, 0
 
-    # BEREKEN HIERONDER DE JUISTE METRIEKEN EN RETOURNEER DIE 
-    # ALS EEN DICTIONARY
+    for i in range(len(metrics)):
+        tp += metrics[i][1]
+        fp += metrics[i][2]
+        fn += metrics[i][3]
+        tn += metrics[i][4]
 
-    rv = {'tpr':0, 'ppv':0, 'tnr':0, 'fpr':0 }
+    tpr = tp / (tp + fn)
+    ppv = tp / (tp + fp)
+    tnr = tn / (tn + fp)
+    fpr = fp / (fp + tn)
+
+    rv = {'tpr':tpr, 'ppv':ppv, 'tnr':tnr, 'fpr':fpr }
     return rv
